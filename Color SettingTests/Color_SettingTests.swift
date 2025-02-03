26d8d193a2a612d73e32efb7ffcb59d48a6744fe
Ilya <fekl13in@gmail.com>
2025-02-03 23:45:30 +0300

    Initial Commit

---
+++ Color SettingTests/Color_SettingTests.swift
@@ -0,0 +1,17 @@
+//
+//  Color_SettingTests.swift
+//  Color SettingTests
+//
+//  Created by Ilya Feklin on 03.02.2025.
+//
+
+import Testing
+@testable import Color_Setting
+
+struct Color_SettingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
