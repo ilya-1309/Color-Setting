268e6aee92586c97bf302ffbfec78ea121c94852
Ilya <fekl13in@gmail.com>
2025-02-04 22:13:52 +0300

    Adding elements UI

--- Color Setting/ViewController.swift
+++ Color Setting/ViewController.swift
@@ -7,11 +7,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var colorDisplay: UIView!
+    // нужен ли отступ?
+    @IBOutlet var redColor: UISlider!
+    @IBOutlet var greenColor: UISlider!
+    @IBOutlet var blueColor: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
